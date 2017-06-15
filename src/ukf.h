e99edf9be80d71830334113c61c0cc3d1294af6b
wenpliu <wen.p.liu@gmail.com>
2017-06-14 22:25:36 -0700

    updated UKF(), Prediction() and UpdateRadar() with lesson code.

--- src/ukf.h
+++ src/ukf.h
@@ -67,6 +67,12 @@ public:
   ///* Sigma point spreading parameter
   double lambda_;
 
+  ///* NIS for radar
+  double NIS_radar_;
+
+  ///* NIS for laser
+  double NIS_laser_;
+
 
   /**
    * Constructor
