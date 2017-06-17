a6ca3e4922ad63b8909b752f5de1066bf9cf5df5
wenpliu <wen.p.liu@gmail.com>
2017-06-17 16:24:51 -0700

    added lidar and updated initialization.

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -27,10 +27,10 @@ UKF::UKF() {
     P_.setIdentity(5,5);
 
     // Process noise standard deviation longitudinal acceleration in m/s^2
-    std_a_ = 30;
+    std_a_ = 2;
 
     // Process noise standard deviation yaw acceleration in rad/s^2
-    std_yawdd_ = M_PI/8;
+    std_yawdd_ = 0.3;
 
     // Laser measurement noise standard deviation position1 in m
     std_laspx_ = 0.15;
@@ -275,7 +275,72 @@ void UKF::UpdateLidar(MeasurementPackage meas_package) {
 
   You'll also need to calculate the lidar NIS.
   */
+    // set measurement dimension, lidar uses x, y
+    int n_z = 2;
 
+    //create example matrix with sigma points in measurement space
+    MatrixXd Zsig = MatrixXd(n_z, 2*n_aug_+1);
+    //transform sigma points into measurement space
+    for (int i = 0; i < 2*n_aug_+1; i++) {
+        Zsig(0,i) = Xsig_pred_(0,i);
+        Zsig(1,i) = Xsig_pred_(1,i);
+    }
+
+    //create example vector for mean predicted measurement
+    VectorXd z_pred = VectorXd(n_z);
+    z_pred.fill(0.0);
+    for (int i=0; i < 2*n_aug_+1; i++) {
+        z_pred = z_pred + weights_(i) * Zsig.col(i);
+    }
+
+    //measurement covariance matrix S
+    MatrixXd S = MatrixXd(n_z,n_z);
+    S.fill(0.0);
+    for (int i = 0; i < 2 * n_aug_ + 1; i++) {  //2n+1 simga points
+        //residual
+        VectorXd z_diff = Zsig.col(i) - z_pred;
+
+        S = S + weights_(i) * z_diff * z_diff.transpose();
+    }
+
+    // add measurement noise covariance matrix
+    MatrixXd R = MatrixXd(n_z, n_z);
+    R << std_laspx_*std_laspx_, 0,
+            0, std_laspy_*std_laspy_;
+
+    S = S + R;
+
+    // incoming lidar measurement
+    VectorXd z = VectorXd(n_z);
+    z << meas_package.raw_measurements_(0),
+            meas_package.raw_measurements_(1);
+
+    // create matrix for cross correlation
+    MatrixXd Tc = MatrixXd(n_x_, n_z);
+    Tc.fill(0.0);
+    //calculate cross correlation matrix
+    for (int i = 0; i < 2*n_aug_+1; i++) {
+        // residual
+        VectorXd z_diff = Zsig.col(i) - z_pred;
+
+        // state diff
+        VectorXd x_diff = Xsig_pred_.col(i) - x_;
+
+        Tc = Tc + weights_(i) * x_diff * z_diff.transpose();
+    }
+
+    //Kalman gain K;
+    MatrixXd K = Tc * S.inverse();
+
+    // residual
+    VectorXd z_diff = z - z_pred;
+
+    // update state mean and covariance matrix
+    x_ = x_ + K * z_diff;
+    P_ = P_ - K * S * K.transpose();
+
+    // calculate NIS
+    NIS_laser_ = z_diff.transpose() * S.inverse() * z_diff;
 
 }
 
